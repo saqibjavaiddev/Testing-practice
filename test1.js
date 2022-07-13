12237f69b4f311c7caccd36bb8e6e14fc0366181
msaqibjavaid <duckyprogrammer@gmail.com>
2022-07-13 17:46:39 +0500

    Testing practice added test #1

---
+++ test1.js
@@ -0,0 +1,9 @@
+const stringLength = (string) => {
+    if(string.length > 1 && string.length < 10) 
+    return string.length
+    else {
+        throw new Error ("the string is less than 1 character long or longer than 10")
+    }
+}
+
+module.exports = stringLength;
---
+++ tests/test1.test.js
@@ -0,0 +1,9 @@
+const stringLength = require('../test1.js')
+
+test ('expected length for abcdsaqib to be 5', () => {
+    expect(stringLength('saqib')).toBe(5);
+})
+
+test('expected to throw the Error', () => {
+    expect(() => {stringLength('saqibjavaid')}).toThrow();
+})
